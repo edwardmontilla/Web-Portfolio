2e2c184ac85a477aa853bc3496a8d238d06507f2
Edward Montilla <edward.montilla@edu.sait.ca>
2024-04-25 16:54:28 -0600

    made slight changes to footer and site name

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className="fixed bottom-0 w-full">
             <div className="bg-gray-500 w-full p-5 text-center">
-                <p className="text-white">©️ {new Date().getFullYear()}{" "}
-                <span>Edward Montilla</span>
-                </p>
+                <p className="text-white">©️ {new Date().getFullYear()}{" "} Edward Montilla</p>
             </div>
         </footer>
     );
