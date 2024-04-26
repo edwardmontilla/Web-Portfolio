c01c55d584ed10f73f95a1b0ae999e78fc8a8f7c
Edward Montilla <edward.montilla@edu.sait.ca>
2024-04-26 15:55:26 -0600

    added more notes

--- app/page.js
+++ app/page.js
@@ -4,18 +4,31 @@ import React from "react";
 import "./globals.css";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
+
+//NOTES:
+// mx-auto to center the image
+// right/left-0 to align to right or left
+// inset-x-0 to align to center
+// you can follow up with mr/ml
+
+// parallax speed should increase as you go down the page
+// parallax last layer should be start to slow down the speed
+
 export default function Home() {
+  
   return (
     <>
-      <Parallax pages={3} className="text-center">
-        <ParallaxLayer speed={0.1}>
-          <img src="/images/sushi_bake.png" alt="Sushi Bake" className="block w-1/3 h-auto absolute left-0" />
-        </ParallaxLayer>
-        
-        <ParallaxLayer offset={0.3} speed={0.8}>
-        <img src="/images/Spam_Musubi.png" alt="Spam Musubi" className="block w-1/3 h-auto absolute right-0" />
-        </ParallaxLayer>
-      </Parallax>
+      <section className="bg-primary">
+        <Parallax pages={2} className="text-center">
+          <ParallaxLayer offset={0} speed={0.1}>
+            <img src="/images/sushi_bake.png" alt="Sushi Bake" className="block w-1/2 absolute inset-x-0 mx-auto" />
+          </ParallaxLayer>
+          
+          <ParallaxLayer offset={0} speed={0.2}>
+          <img src="/images/Spam_Musubi.png" alt="Spam Musubi" className="block w-1/2" />
+          </ParallaxLayer>
+        </Parallax>
+      </section>
     </>
   );
 }
--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 w-full">
-            <div className="bg-gray-500 w-full p-5 text-center">
+        <footer className="absolute bottom-0 w-full">
+            <div className="bg-primary p-5 text-center">
                 <p className="text-white">©️ {new Date().getFullYear()}{" "} Edward Montilla. All Rights Reserved.</p>
             </div>
         </footer>
