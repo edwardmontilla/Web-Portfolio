9cc47db894b1f29ae06a7f181a9e712137d1a0f3
edwardmontilla <119634477+edwardmontilla@users.noreply.github.com>
2024-04-28 01:41:13 -0700

    adjusted offset
    Testing welcome comp

--- components/ParallaxComp.js
+++ components/ParallaxComp.js
@@ -10,23 +10,23 @@ const ParallaxComp = () => {
         <div className="overflow-hidden">
             <Parallax pages={2} className="overflow-hidden">
 
-                <ParallaxLayer offset={0} speed={0.1}>
+                <ParallaxLayer offset={0.25} speed={0.1}>
                 <img src="/images/wave-5.svg" alt="wave 5" className="w-full mt-[-158px]" />
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={0} speed={0.2}>
+                <ParallaxLayer offset={0.25} speed={0.2}>
                 <img src="/images/wave-4.svg" alt="wave 4" className="w-full mt-[-120px]" />
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={0} speed={0.32}>
+                <ParallaxLayer offset={0.25} speed={0.32}>
                 <img src="/images/wave-3.svg" alt="wave 3" className="w-full mt-[-108px]" />
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={0} speed={0.45}>
+                <ParallaxLayer offset={0.25} speed={0.45}>
                 <img src="/images/wave-2.svg" alt="wave 2" className="w-full mt-[-45px]" />
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={0} speed={0.58}>
+                <ParallaxLayer offset={0.25} speed={0.58}>
                 <img src="/images/wave-1.svg" alt="wave 1" className="w-full mt-[-45px]" />
                 </ParallaxLayer>
 
---
+++ components/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// adjust z index
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold text-text z-50">Welcome</h1>
+        </div>
+    )
+}
+
+export default Welcome;
