9ef4f749d7ba6c409122ce036da9f9ccf94901c1
edwardmontilla <119634477+edwardmontilla@users.noreply.github.com>
2024-05-02 23:12:37 -0700

    added responsive texts

--- components/Page2.js
+++ components/Page2.js
@@ -5,9 +5,9 @@ const Page2 = () => {
     return (
         <div id="page2-block">
             <div id="page2-container" className="">
-                <p className="text-lg font-bold text-text text-white mr-80 text-center">Hello</p>
+                <p className="sm:text-sm md:text-md lg:text-lg font-bold text-gray-100 mr-80 text-center">Hello</p>
                     <img src="./next.svg" alt="Next.js Logo" id="rbox" />
-                <p className="text-lg font-bold text-text text-white text-center">Baobao...Test test</p>
+                <p className="sm:text-sm md:text-md lg:text-lg font-bold text-gray-100 text-center">Baobao...Test test</p>
             </div>
             <Footer />
         </div>
