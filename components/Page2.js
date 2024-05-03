33acda7f844083b9f6ab68881dd168cb0f075408
edwardmontilla <119634477+edwardmontilla@users.noreply.github.com>
2024-05-02 23:50:15 -0700

    added text sizes

--- components/Page2.js
+++ components/Page2.js
@@ -5,9 +5,9 @@ const Page2 = () => {
     return (
         <div id="page2-block">
             <div id="page2-container" className="">
-                <p className="sm:text-sm md:text-md lg:text-lg font-bold text-gray-100 mr-80 text-center">Hello</p>
+                <p className="sm:text-sm md:text-base lg:text-lg xl:text-xl font-bold text-gray-100 mr-80 text-center">Hello</p>
                     <img src="./next.svg" alt="Next.js Logo" id="rbox" />
-                <p className="sm:text-sm md:text-md lg:text-lg font-bold text-gray-100 text-center">Baobao...Test test</p>
+                <p className="sm:text-sm md:text-base lg:text-lg xl:text-xl font-bold text-gray-100 text-center">Baobao...Test test</p>
             </div>
             <Footer />
         </div>
