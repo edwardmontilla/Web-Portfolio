8ed44a415246df41bd1186fc4e8719f1d02b84c8
edwardmontilla <119634477+edwardmontilla@users.noreply.github.com>
2024-05-02 21:37:07 -0700

    Update ParallaxComp.js

--- components/ParallaxComp.js
+++ components/ParallaxComp.js
@@ -26,8 +26,8 @@ const ParallaxComp = () => {
 {/* Page 1 */}
 {/* Welcome text */}
             <ParallaxLayer offset={0.2} speed={0.32}>
-                <div className="absolute left-0 top-0 transform -translate-y-1/2 ml-40 sm:ml-20">
-                    <p className="text-6xl font-bold text-text animate-slideFadeRight sm:text-xl">Welcome!</p>
+                <div className="absolute left-0 top-0 transform -translate-y-1/2 ml-40">
+                    <p className="text-6xl font-bold text-text animate-slideFadeRight">Welcome!</p>
                 </div>
             </ParallaxLayer>
 
