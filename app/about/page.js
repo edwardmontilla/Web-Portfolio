efd5b12f00be0de024b3ef67a16916a2da4c2688
Edward Montilla <edward.montilla@edu.sait.ca>
2024-05-23 21:27:33 -0600

    fixed ' apos deployment error

--- app/about/page.js
+++ app/about/page.js
@@ -12,7 +12,7 @@ const About = () => {
                     <h1 className="mt-6 mb-6 text-2xl font-bold lnFont">About Me</h1>
                     <h2 className="text-lg font-semibold lnFont">So what made me pursue a career in software development?</h2>
                         <p id="bio" className="metropolisR">
-                        I hadn't thought that I would go into software development but I was always interested in technology. 
+                        I hadn&apos;t thought that I would go into software development but I was always interested in technology. 
                         What inspired me to pursue a career in software development was the ability to create something from scratch and see it come to life. 
                         I love the idea of creating something that can be used by others and make their lives easier. 
                         While the problem-solving aspect of software development may not seem as fun at first glance, the gratification and constant learning that 
@@ -20,8 +20,8 @@ const About = () => {
                         </p>
                         <p id="bio" className="metropolisR">
                         In addition to my technical and artistic skills, I also enjoy spending time outdoors. I play badminton on a weekly basis and love going on hikes to explore new places. 
-                        Additionally, I'm an avid gamer and a huge fan of illustrations from both video games and animation. 
-                        I'm also planning to get back into aquascaping, creating a small underwater landscape to soothe my mind.
+                        Additionally, I&apos;m an avid gamer and a huge fan of illustrations from both video games and animation. 
+                        I&apos;m also planning to get back into aquascaping, creating a small underwater landscape to soothe my mind.
                         </p>
                 </div>
                 <div id="img-container" className="">
