4c6db41b532f0c95db90ea9dda28cb175b29381d
edwardmontilla <119634477+edwardmontilla@users.noreply.github.com>
2024-05-24 04:31:09 -0700

    added hobby section, revised some sentences

--- app/about/page.js
+++ app/about/page.js
@@ -25,26 +25,25 @@ const About = () => {
                         While the problem-solving aspect of software development may not seem as fun at first glance, the gratification and constant learning that 
                         come with it are what fuel my passion for it. I am always looking for new challenges and ways to improve my skills.
                         </p>
+                    <h2 className="text-lg font-semibold lnFont">Hobbies and Interests</h2>
                         <p id="bio" className="metropolisR">
                         In addition to my technical and artistic skills, I also enjoy spending time outdoors. I play badminton on a weekly basis and love going on hikes and camping to explore new places. 
-                        Additionally, I&apos;m an avid gamer and a huge fan of illustrations from both video games and animation. 
-                        I&apos;m also planning to get back into aquascaping, creating a small underwater landscape to soothe my mind.
+                        Additionally, I thoroughly enjoy gaming as a hobby and find it to be a huge source of inspiration for my creativity. Whether it&apos;s the intricate designs of game environments or 
+                        the captivating characters in animations, I&apos;m constantly drawn to the artistic aspects of gaming and animation.
+                        I&apos;m also planning to get back into aquascaping so I can create a small underwater landscape to soothe my mind.
                         </p>
                 </div>
                 
                 <div id="img-container" className="">
-
                         <img src="/images/jp1.jpg" alt="cat" className="rounded-2xl shadow-gray-400" />
 
                         <div id="hobby-icon" className="gap-4 text-gray-700 bg-background rounded-xl animate-slideFadeLeft shadow-gray-400">
-
                             <GoFileCode className="w-8 h-8 m animate-bounce" />
                             <TfiGame className="w-8 h-8 m animate-bounce animation-delay-300" />
                             <PiPersonSimpleHike className="w-8 h-8 m animate-bounce animation-delay-500"/>
                             <img src="/icons/badminton.svg" alt="badminton" className="w-8 h-8 m animate-bounce animation-delay-700" />
                             <IoColorPaletteOutline className="w-8 h-8 m animate-bounce animation-delay-900" />
                             <LiaFishSolid className="w-8 h-8 m animate-bounce animation-delay-1100" />
-                            
                         </div>
                 </div>
 
