6d1c1b2d77f684e00f5e88763370d99f7bb335a0
edwardmontilla <119634477+edwardmontilla@users.noreply.github.com>
2024-05-28 00:42:12 -0700

    added border

--- app/projects/page.js
+++ app/projects/page.js
@@ -8,7 +8,7 @@ const Projects = () => {
         <>
         <div id="proj" className="">  {/* this is the container for the projects */}
             <div id="proj-img" className="">
-                <img src="/images/mg-1.png" alt="miggy-logo" className="w-1/6 h-auto"/>
+                <img src="/images/mg-1.png" alt="miggy-logo" className="w-1/6 h-auto border rounded-xl border-primary"/>
                 <h2 className="lnFont">Miggy&apos;s Munchies</h2>
                 <p className="metropolisR">Miggy&apos;s Munchies is a food ordering webapp where customers can order food via pickup or delivery. This was created to.....as a capstone project.....with team..</p>
             </div>
