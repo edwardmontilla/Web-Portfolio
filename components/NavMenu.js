883775654c75e7563fb96469352c0e1670b6b557
Edward Montilla <edward.montilla@edu.sait.ca>
2024-05-30 17:59:02 -0600

    adjusted img container to fit 1536px

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -7,15 +7,15 @@ import Link from "next/link";
 
 const NavMenu = () => {
     return (
-        <nav className="flex items-center justify-between ml-8 text-white text-nowrap">
-            <div className="pl-2 pr-2 ml-2 mr-2 border-l-2 border-r-2 border-white metropolisL">
+        <nav className="flex items-center justify-between ml-24 text-white text-nowrap">
+            <div className="pl-2 pr-2 ml-2 mr-2 border-b-2 border-r-2 border-white border-double rounded-br-md metropolisL">
                 <Link href="/">
                     Edward Montilla
                 </Link>
             </div>
             
 
-            <div className="flex justify-end gap-4 mr-8 text-center metropolisL">
+            <div className="flex justify-end gap-4 mr-24 text-center metropolisL">
 
                 <div className="p-2 duration-100 ease-in-out hover:font-semibold hover:text-accent">
                     <Link href="/"> {/* page.js */}
@@ -24,7 +24,7 @@ const NavMenu = () => {
                 </div>
                 <div className="p-2 duration-100 ease-in-out hover:font-semibold hover:text-accent">
                     <Link href="./about#container"> {/* Description about me on a separate page, might include photos of hobbies  */}
-                        About Me
+                        About
                     </Link>
                 </div>
                 <div className="p-2 duration-100 ease-in-out hover:font-semibold hover:text-accent">
