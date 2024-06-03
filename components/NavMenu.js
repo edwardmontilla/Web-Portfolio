da12ca26324fc420cfd5a0ecbecdf42fe64ff2c3
Edward Montilla <edward.montilla@edu.sait.ca>
2024-06-02 22:21:14 -0600

    tried fixing name on mobile

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -8,9 +8,14 @@ import Link from "next/link";
 const NavMenu = () => {
     return (
         <nav className="flex items-center justify-between ml-16 text-white text-nowrap">
-            <div className="pl-2 pr-2 ml-2 mr-2 border-r border-white metropolisL">
+            <div className="pl-2 pr-2 ml-2 mr-2 border-r border-white metropolisL" id="desktop-name-logo">
                 <Link href="/">
                     Edward Montilla
+                </Link>
+            </div>
+            <div className="metropolisL" id="name-logo">
+                <Link href="/">
+                    EM
                 </Link>
             </div>
             
